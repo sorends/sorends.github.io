c1bdb782f5f0e8aea289c8105b9573e7339c0aab
Nitro <soren.ds@gmail.com>
2025-02-07 20:44:21 +0100

    PWA setup.

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'your-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/runa.png'
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
