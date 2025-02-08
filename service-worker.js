c792a7e37c02dc05a3a79876ded3de18f3545218
Nitro <soren.ds@gmail.com>
2025-02-08 19:59:37 +0100

    .

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
-const CACHE_NAME = 'your-app-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/runa.png'
-  // Add other assets you want to cache
-];
+// const CACHE_NAME = 'your-app-cache-v1';
+// const urlsToCache = [
+//   '/',
+//   '/index.html',
+//   '/runa.png'
+//   // Add other assets you want to cache
+// ];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
